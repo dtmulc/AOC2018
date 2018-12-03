4d78a793b71177d79da5547cbfb5cc96521a59e4
dtmulc <dtmulchy@gmail.com>
2018-12-03 06:49:09 -0500

    used an older version the first time

--- p1.py
+++ p1.py
@@ -1,31 +1,33 @@
-import sys, itertools
-def freq(infil):
+import sys
+def freq_func(infil):
 	i = 0
+	j = 0
 	freq_chk = 0
 	freq_reached = []
 	freq_reached.append(0)
-	# eval_str = '0'
+	eval_str = '0'
 	# sec_count = 0
 	data = open(infil, 'r+')
 	freqs = data.readlines()
 	for line in freqs:
 		freqs[i] = line.strip()
 		i += 1
-	# eval_str += ' '.join(freqs)
-	# print(eval(eval_str))
+	eval_str += ' '.join(freqs)
+	print(eval(eval_str))
+	
+	#failed attempt at part 2. I think logically it makes sense but I keep running into weird time out issues running it in windows cmd
 	while True:
-		for freq in itertools.cycle(freqs):
+		for freq in freqs:
 			freq = int(freq)
 			freq_chk += freq
 			if freq_chk in freq_reached:
 				return(freq_chk)
 			else:
 				freq_reached.append(freq_chk)
-				print(len(freq_reached))
+				# print(len(freq_reached))
 		# sec_count += 1
 		# print(freq_reached)
 		# print()
 	# print(sorted(freq_reached))
 
-infil = sys.argv[1]
-freq(infil)
+freq_func(sys.argv[1])
